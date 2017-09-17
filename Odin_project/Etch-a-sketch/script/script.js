78d2eb06cfdf0ca7798e6d368653d67d39ca6be2
Michelle <michellebrettcr@gmail.com>
2017-09-17 21:36:14 +1000

    Add Content

---
+++ Odin_project/Etch-a-sketch/script/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var size = 16; /* set default size */
+  $('#size').on('click', function() {
+    size = prompt("How many squares wide?"); 
+  });
+  /* fill grid using default or set size - how will this rerun each time size button is reclicked */
+  
+  /* add class to square that is hovered over - default and using color button*/
+  
+  /* add opacity class when button clicked */
+  
+  /* clear entire grid of color */  
+});
