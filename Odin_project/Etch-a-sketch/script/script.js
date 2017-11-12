4b10f16d75b8f88a8c5380f64bc00ab948fe2ef0
Michelle <michellebrettcr@gmail.com>
2017-11-12 19:25:21 +1100

    Add ability to change box number

--- Odin_project/Etch-a-sketch/script/script.js
+++ Odin_project/Etch-a-sketch/script/script.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
-  var size = 16; /* set default size */ 
-  var squareSize = Math.floor(445/size) + "px";
-	console.log(squareSize);
+  var size = 16;
+  loadSquare(size); 
 
-  for (var i=0; i<size; i++){
-	  for (var j=0; j<size; j++){
-		 $('#box').append("<div class='squares'></div>");
-	  }
-  };
+
+function loadSquare(size){
+	var squareSize = Math.floor(445/size) + "px";
+
+	for (var i=0; i<size; i++){
+        for (var j=0; j<size; j++){
+			 $('#box').append("<div class='squares'></div>");
+	    };
+	 }
+
+	 $('.squares').css({'height': squareSize, 'width': squareSize});
+}
 	
-$('.squares').css({'height': squareSize, 'width': squareSize});;
+$('#size').on('click', function() {
+    var size = prompt("How many squares wide?");
+	$('#box').empty();
+	loadSquare(size);
+});
 
-  
-  /* Set size of divs*/	
- /* $('#size').on('click', function() {
-    size = prompt("How many squares wide?"); 
-  });*/
   
   /* fill grid using default or set size - how will this rerun each time size button is reclicked */
   /* add class to square that is hovered over - default and using color button*/
