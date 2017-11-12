89d3bec9adac234b8572598854f282163d78a611
Michelle <michellebrettcr@gmail.com>
2017-11-12 19:13:19 +1100

    Add Div loading

--- Odin_project/Etch-a-sketch/script/script.js
+++ Odin_project/Etch-a-sketch/script/script.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
   var size = 16; /* set default size */ 
-  $('#size').on('click', function() {
+  var squareSize = Math.floor(445/size) + "px";
+	console.log(squareSize);
+
+  for (var i=0; i<size; i++){
+	  for (var j=0; j<size; j++){
+		 $('#box').append("<div class='squares'></div>");
+	  }
+  };
+	
+$('.squares').css({'height': squareSize, 'width': squareSize});;
+
+  
+  /* Set size of divs*/	
+ /* $('#size').on('click', function() {
     size = prompt("How many squares wide?"); 
-  });
+  });*/
   
   /* fill grid using default or set size - how will this rerun each time size button is reclicked */
-  
   /* add class to square that is hovered over - default and using color button*/
   
   /* add opacity class when button clicked */
