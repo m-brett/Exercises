4979016710530418884728b7c5b0b1e538b035ad
Michelle <michellebrettcr@gmail.com>
2017-11-19 15:48:55 +1100

    Completed Styling

--- Free_code_camp/Pomodoro/script/script.js
+++ Free_code_camp/Pomodoro/script/script.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
 
+	/* SIDE BUTTONS */
+	$('#open').click(function(){
+		$('#opts_cover').animate({top: 130}, 500);
+	});
 
-$('#open').click(function(){
-	$('#opts_cover').animate({top: 130}, 500);
-});
+	$('#close').click(function(){
+		$('#opts_cover').animate({top: 0}, 500);
+	});
 	
-$('#close').click(function(){
-	$('#opts_cover').animate({top: 0}, 500);
-});
+	
+	var startTime = 25
+	
+	/* MAIN CONTROLS */
+	$('#start').click(function(){
+		/* if different start time isn't set, countdown from 25 */
+		/* At 0, change time to five minutes and text to rest */
+		/* Repeat */
+	});
+	
+	$('#pause').click(function(){
+		/* Pause Timer */
+	});
+	
+	$('stop').click(function(){
+		/* Rest time to 25 */
+	});
+	
+	
+	
 
 	
 });
